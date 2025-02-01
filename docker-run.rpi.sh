512964e0673ca1b169520496966060abcba0f1ad
se1exin <selexin@selexin.com>
2025-02-01 16:31:00 +1100

    Update docker scripts for raspberry pi

--- docker-run.rpi.sh
+++ docker-run.rpi.sh
@@ -1,4 +1,9 @@
 docker run -it \
   --privileged \
+  --net=host \
+  -v /dev:/dev/ \
+  -v /run/udev/:/run/udev/ \
   robot-tank-rpi \
-  ros2 run rpi_comm rpi_comm
+  ros2 run realsense2_camera realsense2_camera_node
+
+  #   ros2 run rpi_comm rpi_comm
--- rpi.sh
+++ rpi.sh
@@ -2,7 +2,4 @@
 
 source ./install/setup.bash
 
-# Needed for docker communication
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-
 ros2 run rpi_comm rpi_comm
