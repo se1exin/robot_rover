256c7df61a6b37ebec5429812a8261698e377c6d
selexin <selexin@selexin.com>
2025-02-07 17:13:51 +1100

    Update rpi docker launch script

--- docker-run.rpi.sh
+++ docker-run.rpi.sh
@@ -1,9 +1,8 @@
-# docker container rm robot-tank-rpi
-# --name="robot-tank-rpi" \
+docker container rm robot-tank-rpi
 
 SERVER_IP_ADDRESS=10.1.1.6
 
-docker run --rm -it \
+docker run -it -d \
   --privileged \
   --net=host \
   -e ROS_STATIC_PEERS=$SERVER_IP_ADDRESS \
@@ -12,6 +11,8 @@ docker run --rm -it \
   -v /dev:/dev/ \
   -v /run/udev/:/run/udev/ \
   -v $(pwd)/src:/app/src \
+  --restart=unless-stopped \
+  --name="robot-tank-rpi" \
   robot-tank-rpi \
   ros2 launch rpi rpi.launch.py
   #bash
