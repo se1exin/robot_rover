c081b27a2ea1db1538db507d6e6c3bbaf4fb721e
selexin <selexin@selexin.com>
2025-03-14 07:32:18 +0000

    Update cameras to use rpi cam v3

--- src/rpi/launch/rpi.cam1.launch.py
+++ src/rpi/launch/rpi.cam1.launch.py
@@ -10,7 +10,11 @@ def generate_launch_description():
         output='screen',
         parameters=[
             {
-                "camera": "/base/axi/pcie@120000/rp1/i2c@88000/ov5647@36",
+                # "camera": "/base/axi/pcie@120000/rp1/i2c@88000/ov5647@36", # Rpi Camera v1.3
+                "camera": "/base/axi/pcie@120000/rp1/i2c@88000/imx708@1a",  # Rpi Cam v3
+                "format": "XRGB8888",
+                "width": 1280,
+                "height": 1024
             },
         ]
     )
--- src/rpi/launch/rpi.cam2.launch.py
+++ src/rpi/launch/rpi.cam2.launch.py
@@ -10,7 +10,10 @@ def generate_launch_description():
         output='screen',
         parameters=[
             {
-                "camera": "/base/axi/pcie@120000/rp1/i2c@80000/ov5647@36",
+                "camera": "/base/axi/pcie@120000/rp1/i2c@80000/ov5647@36",  # Rpi NoIR Camera v1.3
+                "format": "XRGB8888",
+                "width": 1280,
+                "height": 1024
             },
         ]
     )
